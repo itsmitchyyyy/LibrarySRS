d1ee244ee08e1f1ba88947a090e5679c45b4fb86
Mitch Alforque <malforque.mynimo@gmail.com>
2021-02-24 19:55:10 +0800

    initial commit

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+    <div class="mt-5 d-flex  footerContainer">
+        <div class="d-flex justify-content-center flex-column flex-fill align-items-center">
+            <span>HAVE ANY QUESTIONS? CALL OR TEXT US</span>
+            <span style="font-size:12px">© 2021 CEBU EASTERN COLLEGE LIBRARY, ALL RIGHTS RESERVED</span>
+        </div>
+    </div>
+</body>
+</html>
