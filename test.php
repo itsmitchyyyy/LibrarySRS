fec9708560370d1bb857edf752d04f5b44ae6350
Mitch Alforque <malforque717@gmail.com>
2021-04-19 23:57:46 +0800

    test page

---
+++ test.php
@@ -0,0 +1,8 @@
+<?php require 'db/connection.php';
+
+    $username = "test";
+    $password =  password_hash("P@ssw0rd!p", PASSWORD_DEFAULT);
+  
+
+    addRecord(array($username,$password), array('username','password'), 'users');
+    header('location:index.php');
