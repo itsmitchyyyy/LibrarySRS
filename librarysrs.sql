bb0b3d1c7fd3d8775cc2df380d1ecea18cbd9101
Mitch Alforque <malforque717@gmail.com>
2021-04-22 08:39:00 +0800

    a

--- librarysrs.sql
+++ librarysrs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 22, 2021 at 03:15 PM
+-- Generation Time: Apr 21, 2021 at 06:48 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.14
 
@@ -20,6 +20,119 @@ SET time_zone = "+00:00";
 --
 -- Database: `librarysrs`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `ddc` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `edition_number` varchar(255) NOT NULL,
+  `place_of_publication` varchar(255) NOT NULL,
+  `publisher` varchar(255) NOT NULL,
+  `copyright` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'published',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `ddc`, `description`, `author`, `edition_number`, `place_of_publication`, `publisher`, `copyright`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Book title', 'Descriptionaaaajjj', 'Author', '1212', 'bisag asa', 'bisag aaaa', '12', 'published', '2021-04-20 14:57:04', '2021-04-20 15:30:33'),
+(2, 'zxc', 'zxczxczxc', 'zxc', '12121', 'asasd', 'asasdasd', '111', 'disabled', '2021-04-20 15:03:09', '2021-04-20 15:45:18'),
+(3, 'Book title', 'qqq', 'Author', '12121', 'asasd', 'bisag aaaa', '12', 'disabled', '2021-04-20 15:45:32', '2021-04-20 15:54:08'),
+(4, 'zxczxczxczxqqqqq', 'casdasd', 'Author', '12121', 'asasd', 'asasdasd', '12', 'disabled', '2021-04-20 15:46:01', '2021-04-20 16:21:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `ddc` int(11) NOT NULL,
+  `alias` varchar(100) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `ddc`, `alias`, `name`, `description`, `created_at`, `updated_at`) VALUES
+(1, 1212, 'Category 1', 'Alias 1', 'Description 111111', '2021-04-20 14:38:19', '2021-04-20 14:38:19'),
+(2, 11111, 'Category 2', 'Alias 2', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', '2021-04-20 14:42:55', '2021-04-20 14:42:55');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `library_staffs`
+--
+
+CREATE TABLE `library_staffs` (
+  `id` int(11) NOT NULL,
+  `id_number` int(11) NOT NULL,
+  `first_name` varchar(150) NOT NULL,
+  `middle_name` varchar(20) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
+  `contact_number` int(20) NOT NULL,
+  `course` varchar(150) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penalties`
+--
+
+CREATE TABLE `penalties` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amount` varchar(255) NOT NULL,
+  `due_date` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `return_date` timestamp NULL DEFAULT NULL,
+  `approved_date` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reservations`
+--
+
+INSERT INTO `reservations` (`id`, `book_id`, `user_id`, `status`, `return_date`, `approved_date`, `created_at`, `updated_at`) VALUES
+(1, 1, 259, 'returned', '2021-04-21 10:46:56', '2021-04-21 10:41:28', '2021-04-21 14:51:47', '2021-04-21 14:51:47');
 
 -- --------------------------------------------------------
 
@@ -67,9 +180,19 @@ CREATE TABLE `students` (
   `middle_name` varchar(20) NOT NULL,
   `last_name` varchar(150) NOT NULL,
   `contact_number` int(20) NOT NULL,
-  `course` int(150) NOT NULL,
-  `email` varchar(100) NOT NULL
+  `course` varchar(150) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `id_number`, `first_name`, `middle_name`, `last_name`, `contact_number`, `course`, `email`, `status`, `created_at`, `updated_at`) VALUES
+(253, 12345651, 'asasdad', 'qwe', 'qqwewqzzz', 123123, 'qqwewq', 'emailasdasdasdaddress@gmail.com', 'active', '2021-04-21 14:23:39', '2021-04-21 16:09:57');
 
 -- --------------------------------------------------------
 
@@ -85,7 +208,10 @@ CREATE TABLE `teachers` (
   `last_name` varchar(150) NOT NULL,
   `email` varchar(100) NOT NULL,
   `department` varchar(100) NOT NULL,
-  `contact_number` varchar(20) NOT NULL
+  `contact_number` varchar(20) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -97,12 +223,55 @@ CREATE TABLE `teachers` (
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` text NOT NULL
+  `password` text NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`, `role_id`, `status`, `created_at`, `updated_at`) VALUES
+(259, '12345651', '$2y$10$DwMwPDLAxdXNOoX/RuYEDeisaan7S62LNU3JizfnsOZCXv8xmouvC', 'students', 253, 'active', '2021-04-21 14:23:39', '2021-04-21 14:23:39'),
+(261, 'test', '$2y$10$oORWuL6cOV5L4b/1dvJfOeMhdjxmSwfr3PtHprqS4PAZnBgcYcN9G', 'admin', 0, 'active', '2021-04-21 16:40:18', '2021-04-21 16:40:18');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `library_staffs`
+--
+ALTER TABLE `library_staffs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `penalties`
+--
+ALTER TABLE `penalties`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `roles`
@@ -123,6 +292,12 @@ ALTER TABLE `students`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -131,6 +306,36 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `library_staffs`
+--
+ALTER TABLE `library_staffs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=254;
+
+--
+-- AUTO_INCREMENT for table `penalties`
+--
+ALTER TABLE `penalties`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `roles`
@@ -142,19 +347,25 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `role_user`
 --
 ALTER TABLE `role_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=254;
+
+--
+-- AUTO_INCREMENT for table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=262;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
